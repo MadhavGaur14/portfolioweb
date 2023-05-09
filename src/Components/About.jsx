c4084f3e91e6b5754ef7a95f3bdc1b1af6726165
Madhav Gaur <117013332+MadhavGaur14@users.noreply.github.com>
2023-05-09 17:29:52 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bluebkg.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an Engineering student studying at N.M.A.M Institute of Technology with a keen interest in Cloud Computing and Coding.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Microsoft Azure",
+  "AWS Cloud Computing",
+  "Python",
+  "Java",
+  "C-Programming",
+  "Technical Content Writing",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "As a passionate technologist with a deep interest in coding and cutting-edge technologies, I am constantly exploring and learning new ways to apply my skills to solve complex problems. My goal is to leverage my skills and expertise to make a positive impact, while continuously growing and expanding my knowledge in this ever-evolving field. I am driven by a love for innovation and a desire to create meaningful solutions that drive real-world results.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/laptop.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "AWS Cloud Computing Notes for Beginners",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A project for students to help in securing placements and a guidance material notes for them.",
+    url: "https://drive.google.com/file/d/1--ke5r66ufInMQokIsXeku155KUg6Tsv/view?usp=sharing",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Ultrasonic Sensor Device for Visually Impaired",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Body Wearable Obstacle and Range Estimation using 555 Timer for Visually Impaired.",
+    url: "https://docs.google.com/presentation/d/1E3Kt74lPg-CVAsJuQVABIb2ngQuWCNa0/edit?usp=sharing&ouid=117129290200278114758&rtpof=true&sd=true",
   },
 ];
 
